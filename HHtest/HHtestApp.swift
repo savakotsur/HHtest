68f7cd52208d274925cc0480f2db902027e03051
savakotsur <slendygood@gmail.com>
2024-05-27 21:59:45 +0300

    Initial Commit

---
+++ HHtest/HHtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHtestApp.swift
+//  HHtest
+//
+//  Created by Савелий Коцур on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HHtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
